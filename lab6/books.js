5ce81236083f035f7db849ec6f7b3142f89f8d5b
Анастасия Витлева <anvitleva@gmail.com>
2025-05-01 15:34:38 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: 'Лао-цзы',
+        name: 'Дао дэ цзин. Канон о Дао и дэ',
+        publicationYear: 2022,
+        price: '450 ₽',
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Карл Саган',
+        name: 'Космос',
+        publicationYear: 2024,
+        price: '750 ₽',
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Баладжи Касал',
+        name: 'Ошибки разумного инвестора',
+        publicationYear: 2025,
+        price: '990 ₽',
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Джордж Оруэлл',
+        name: '1984',
+        publicationYear: 1949,
+        price: '560 ₽',
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Фрэнк Герберт',
+        name: 'Дюна',
+        publicationYear: 1965,
+        price: '880 ₽',
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Айзек Азимов',
+        name: 'Основание',
+        publicationYear: 1951,
+        price: '670 ₽',
+        bestseller: false,
+        available: false
+    },
+    {
+        author: 'Рэй Брэдбери',
+        name: '451° по Фаренгейту',
+        publicationYear: 1953,
+        price: '500 ₽',
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Стивен Хокинг',
+        name: 'Краткая история времени',
+        publicationYear: 1988,
+        price: '720 ₽',
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Юваль Ной Харари',
+        name: 'Sapiens. Краткая история человечества',
+        publicationYear: 2014,
+        price: '930 ₽',
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Майкл Сэндэл',
+        name: 'Чего не купишь за деньги',
+        publicationYear: 2012,
+        price: '610 ₽',
+        bestseller: false,
+        available: true
+    }
+];
